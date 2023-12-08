1992fdd876615502cfd77d88a6370095dedd0fa1
Ofir Patish <ofirpatishop@gmail.com>
2023-12-08 12:54:28 +0200

    Updated TodoList

--- JS/TodoList/script.js
+++ JS/TodoList/script.js
@@ -1,18 +1,55 @@
-function addTask() {
-  var taskName = document.getElementById("taskNameInput").value;
-  var dueDate = document.getElementById("dueDateInput").value;
-
-  if (taskName && dueDate) {
-    var taskItem = document.createElement("li");
-    var checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-
-    taskItem.appendChild(checkbox);
-    taskItem.appendChild(document.createTextNode(taskName + " (Due: " + dueDate + ")"));
-
-    document.getElementById("taskList").appendChild(taskItem);
-
-    document.getElementById("taskNameInput").value = "";
-    document.getElementById("dueDateInput").value = "";
-  }
+var tasks = JSON.parse(localStorage.getItem("myTasks"));
+if (tasks == null) {
+  var tasks = [];
 }
+
+createTable();
+
+function createTable() {
+  var result = `<table border="1" cellSpacing="0" style="margin-left:auto;margin-right:auto">`;
+  for (var index = 0; index < tasks.length; index++) {
+    result += `
+      <tr>
+        <td ${tasks[index].taskDone ? 'style="text-decoration: line-through;"' : ""}>${tasks[index].taskName}</td>
+        <td ${tasks[index].taskDone ? 'style="text-decoration: line-through;"' : ""}>${tasks[index].taskDate}</td>
+        <td><input type="checkbox" ${tasks[index].taskDone ? "checked" : ""} onchange="toggleTaskDone(${index})"/></td>
+      </tr>
+    `;
+  }
+  result += "</table>";
+  document.getElementById("taskList").innerHTML = result;
+}
+
+function Tasks(taskName, taskDate, taskDone) {
+  this.taskName = taskName;
+  this.taskDate = taskDate;
+  this.taskDone = taskDone;
+
+  this.taskIsDone = function () {
+    this.taskDone = true;
+  };
+}
+
+const saveTasks = () => {
+  localStorage.setItem("myTasks", JSON.stringify(tasks));
+};
+
+const addTask = () => {
+  var taskName = document.getElementById("taskName").value;
+  var taskDate = document.getElementById("taskDate").value;
+  tasks.push(new Tasks(taskName, taskDate, false));
+  createTable();
+  saveTasks();
+};
+
+const clearTasks = () => {
+  localStorage.removeItem("myTasks");
+  tasks = [];
+  createTable();
+};
+
+const toggleTaskDone = (index) => {
+  tasks[index].taskDone = !tasks[index].taskDone;
+  createTable();
+  saveTasks();
+};
