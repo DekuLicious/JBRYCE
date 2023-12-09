1714bbfbae586ce1073eaefee5441d6f73fd0110
Ofir Patish <132842216+DekuLicious@users.noreply.github.com>
2023-12-09 23:59:50 +0200

    Updated JS content

--- JS/SimpleDigitChecker/script.js
+++
@@ -1,9 +0,0 @@
-var userNumber = prompt("Please enter a number");
-var digits = 0;
-var temp = userNumber;
-
-while (temp > 0) {
-  digits++;
-  temp = parseInt(temp / 10);
-}
-console.log("User number: ", userNumber, " has ", digits, " digits");
