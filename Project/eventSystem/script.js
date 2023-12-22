6b70b57ddaf8b57057bc7c4009ca5b505846c412
Ofir Patish <132842216+DekuLicious@users.noreply.github.com>
2023-12-22 15:01:07 +0200

    Add event form and event table to index.html, refactor script.js

--- Project/eventSystem/script.js
+++ Project/eventSystem/script.js
@@ -1,86 +1,79 @@
 // Constants
-const eventContainer = document.getElementById("eventContainer");
 const localStorageKey = "events";
 
 // Load events from local storage
-var allEvents = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+let allEvents;
+try {
+  allEvents = JSON.parse(localStorage.getItem(localStorageKey)) || [];
+} catch (error) {
+  console.error("Error parsing events from local storage:", error);
+  allEvents = [];
+}
 
-// HTML template for the page
-const siteTemplate = `
-            <form id="eventForm">
-                    <h1>Event Info</h1>
-                    <input type="text" id="eventName" placeholder="Event Name" required/>
-                    <input type="number" id="minPrice" placeholder="Minimum Price" required/>
-                    <input type="number" id="maxPrice" placeholder="Maximum Price" required/>
-                    <select id="eventType" required>
-                            <option value="">--Please choose an option--</option>
-                            <option value="Wedding">Wedding</option>
-                            <option value="Bar Mitzvah">Bar Mitzvah</option>
-                            <option value="Anniversary">Anniversary</option>
-                            <option value="Birthday">Birthday</option>
-                    </select>
-                    <input type="text" placeholder="Image URL" id="imageUrl" required/>
-                    <input type="date" id="eventDate" required/>
-                    <div class="button-container">
-                            <input type="submit" value="Add Event"/>
-                            <input type="reset" value="Reset Form"/>
-                            <button id="clearEvents" type="button">Clear Events</button>
-                    </div>
-            </form>
-            <div id="eventTable"></div>
-    `;
-eventContainer.innerHTML = siteTemplate;
-
-// Event listeners
-document.getElementById("eventForm").addEventListener("submit", addEvent);
-document.getElementById("clearEvents").addEventListener("click", clearEvents);
-
-// Function to add a new event to the list
-function addEvent(formSubmitEvent) {
-  formSubmitEvent.preventDefault();
-
-  const minPrice = Number(document.getElementById("minPrice").value);
-  const maxPrice = Number(document.getElementById("maxPrice").value);
-
+// Function to validate form input
+const validateInput = (minPrice, maxPrice, eventDate) => {
   if (minPrice > maxPrice) {
     alert("Minimum price cannot be higher than maximum price");
-    return;
+    return false;
   }
 
-  const eventDate = new Date(document.getElementById("eventDate").value);
   const currentDate = new Date();
-
   eventDate.setHours(0, 0, 0, 0);
   currentDate.setHours(0, 0, 0, 0);
 
   if (eventDate < currentDate) {
     alert("Event date cannot be in the past");
-    return;
+    return false;
   }
 
-  const newEvent = {
+  return true;
+};
+
+// Function to create a new event object
+const createNewEvent = () => {
+  return {
     name: document.getElementById("eventName").value,
-    minPrice: minPrice,
-    maxPrice: maxPrice,
+    minPrice: Number(document.getElementById("minPrice").value),
+    maxPrice: Number(document.getElementById("maxPrice").value),
     type: document.getElementById("eventType").value,
     imageUrl: document.getElementById("imageUrl").value,
     date: document.getElementById("eventDate").value,
   };
-  allEvents.push(newEvent);
+};
+
+// Function to update local storage
+const updateLocalStorage = () => {
   localStorage.setItem(localStorageKey, JSON.stringify(allEvents));
+};
+
+// Function to add a new event to the list
+const addEvent = (formSubmitEvent) => {
+  formSubmitEvent.preventDefault();
+
+  const minPrice = Number(document.getElementById("minPrice").value);
+  const maxPrice = Number(document.getElementById("maxPrice").value);
+  const eventDate = new Date(document.getElementById("eventDate").value);
+
+  if (!validateInput(minPrice, maxPrice, eventDate)) {
+    return;
+  }
+
+  const newEvent = createNewEvent();
+  allEvents.push(newEvent);
+  updateLocalStorage();
   createTable();
   document.getElementById("eventForm").reset();
-}
+};
 
 // Function to clear all events
-function clearEvents() {
+const clearEvents = () => {
   allEvents = [];
   localStorage.removeItem(localStorageKey);
   createTable();
-}
+};
 
 // Function to create and update the event table
-function createTable() {
+const createTable = () => {
   if (allEvents.length == 0) {
     document.getElementById("eventTable").innerHTML = "";
     return;
@@ -124,15 +117,19 @@ function createTable() {
   document.querySelectorAll(".delete-button").forEach((button) => {
     button.addEventListener("click", deleteEvent);
   });
-}
+};
 
 // Function to delete an event
-function deleteEvent(deleteEventTrigger) {
+const deleteEvent = (deleteEventTrigger) => {
   const index = deleteEventTrigger.target.getAttribute("data-index");
   allEvents.splice(index, 1);
-  localStorage.setItem(localStorageKey, JSON.stringify(allEvents));
+  updateLocalStorage();
   createTable();
-}
+};
 
 // Initial table creation
 createTable();
+
+// Event listeners
+document.getElementById("eventForm").addEventListener("submit", addEvent);
+document.getElementById("clearEvents").addEventListener("click", clearEvents);
