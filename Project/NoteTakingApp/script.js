48ae76173a9bb1cc7d82a929a8095e3d7fcccdb2
Ofir Patish <132842216+DekuLicious@users.noreply.github.com>
2023-12-24 21:44:01 +0200

    Add new files and update existing files

---
+++ Project/NoteTakingApp/script.js
@@ -0,0 +1,57 @@
+document.getElementById("addNote").addEventListener("click", function () {
+  var noteInput = document.getElementById("noteInput");
+
+  if (noteInput.value.trim() === "") {
+    return;
+  }
+
+  var note = document.createElement("div");
+  note.className = "note";
+
+  var noteText = document.createElement("span");
+  noteText.textContent = noteInput.value;
+  note.appendChild(noteText);
+
+  var deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "deleteButton";
+  deleteButton.addEventListener("click", function () {
+    note.remove();
+    saveNotes();
+  });
+
+  note.appendChild(deleteButton);
+  document.getElementById("notesContainer").appendChild(note);
+  noteInput.value = "";
+
+  saveNotes();
+});
+
+// Save notes to localStorage
+function saveNotes() {
+  try {
+    var noteSpans = document.querySelectorAll(".note span");
+    var notes = [];
+    for (var index = 0; index < noteSpans.length; index++) {
+      notes.push(noteSpans[index].textContent);
+    }
+    if (notes.length === 0) {
+      localStorage.removeItem("notes");
+    } else {
+      localStorage.setItem("notes", JSON.stringify(notes));
+    }
+  } catch (storageError) {
+    console.error("Error saving to localStorage", storageError);
+  }
+}
+
+// Load notes from localStorage
+window.onload = function () {
+  var savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+  savedNotes.forEach((note) => {
+    var noteInput = document.getElementById("noteInput");
+    noteInput.value = note;
+    document.getElementById("addNote").click();
+    noteInput.value = "";
+  });
+};
